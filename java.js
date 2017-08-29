277f823da5d2231a4a2943aba84db19f3049ddad
Takeshi Takashima <takashima-takeshi@ksk.co.jp>
2017-08-29 14:08:08 +0900

    update

--- java.js
+++ java.js
@@ -2,45 +2,64 @@ const fs = require('fs');
 const path = require('path');
 const child_process = require('child_process');
 
+//TODO　data文字列化　文字コード指定
 module.exports = function(param){
     let javaPath = param.javaPath || 'java';
     let jvmOptions = param.jvmOptions || [];
     let classpathArray = param.classpath || [];
     let tempPath = param.tempPath || '';
     let cwdPath = param.cwd || __dirname;
-    let systemOutCallback = param.systemOut;
-    let systemErrorCallback = param.systemError;
-    let endCallback = param.end;
-    let errorCallback = param.error;
+    let isBufferMode = param.bufferMode;
 
     return {
         execute: execute,
-        classpath: classpath,
-        systemOut: systemOut,
-        systemError: systemError,
-        end: end,
-        error: error
+        classpath: classpath
     };
 
     ////////// function
-    function execute(executableJavaPath, paramString){
-        let commandArgs = generateCommandArgs(executableJavaPath, paramString);
-        let javaProcess = child_process.spawn(javaPath, commandArgs, {cwd: cwdPath, env: {TEMP: tempPath}, shell: true});
+    //Javaコマンドを実行する
+    //@require executableJavaPath
+    //第2引数以降：　可変長リストを受け取り、javaコマンドのパラメータとして渡す
+    function execute(executableJavaPath){
+        if(!executableJavaPath){
+            throw new Error('executableJavaPath is required.');
+        }
+
+        let stdoutBuffer = Buffer.from('');
+        let stderrBuffer = Buffer.from('');
+
+        let systemOutCallback = param.systemOut;
+        let systemErrorCallback = param.systemError;
+        let endCallback = param.end;
+        let errorCallback = param.error;
+
+        let argArray = Array.prototype.slice.call(arguments);
+        let args = argArray.length > 1 ? argArray.slice(1) : [];
+
+        let commandArgs = generateCommandArgs(executableJavaPath, args);
+        //shell: false -> パラメータ文字列を空白文字で区切らない　空白文字が含まれていても一つのパラメータとしてjavaに渡す
+        let javaProcess = child_process.spawn(javaPath, commandArgs, {cwd: cwdPath, env: {TEMP: tempPath}, shell: false});
 
         //event
         javaProcess.stdout.on('data', function(data){
+            if(isBufferMode){
+                stdoutBuffer = Buffer.concat([stdoutBuffer, data]);
+            }
             if(systemOutCallback){
                 systemOutCallback(data);
             }
         });
         javaProcess.stderr.on('data', function(data){
+            if(isBufferMode){
+                stderrBuffer = Buffer.concat([stderrBuffer, data]);
+            }
             if(systemErrorCallback){
                 systemErrorCallback(data);
             }
         });
         javaProcess.on('close', function(code){
             if(endCallback){
-                endCallback(code);
+                endCallback(code, stdoutBuffer, stderrBuffer);
             }
         });
         javaProcess.on('error', function(err){
@@ -48,6 +67,34 @@ module.exports = function(param){
                 errorCallback(err);
             }
         });
+
+        return {
+            childProcess: javaProcess,
+            systemOut: function(callback){
+                if(callback){
+                    systemOutCallback = callback;
+                }
+                return this;
+            },
+            systemError: function(callback){
+                if(callback){
+                    systemErrorCallback = callback;
+                }
+                return this;
+            },
+            end: function(callback){
+                if(callback){
+                    endCallback = callback;
+                }
+                return this;
+            },
+            error: function(callback){
+                if(callback){
+                    errorCallback = callback;
+                }
+                return this;
+            }
+        };
     }
     function classpath(pathParam){
         if(pathParam){
@@ -61,27 +108,7 @@ module.exports = function(param){
             }
         }
     }
-    function systemOut(callback){
-        if(callback){
-            systemOutCallback = callback;
-        }
-    }
-    function systemError(callback){
-        if(callback){
-            systemErrorCallback = callback;
-        }
-    }
-    function end(callback){
-        if(callback){
-            endCallback = callback;
-        }
-    }
-    function error(callback){
-        if(callback){
-            errorCallback = callback;
-        }
-    }
-    function generateCommandArgs(executableJavaPath, paramString){
+    function generateCommandArgs(executableJavaPath, args){
         let result = [];
 
         if(jvmOptions && jvmOptions.length > 0){
@@ -92,12 +119,16 @@ module.exports = function(param){
             result.push(classpathArray.join(path.delimiter));
         }
         result.push(executableJavaPath);
-        if(paramString){
-//TODO objectの文字列化
-//TODO executeの　パラメータ 可変長リスト化
-            result = result.concat(paramString.split(' '));
+        if(args && args.length > 0){
+            args.forEach(function(arg){
+                if(typeof(arg) === 'string'){
+                    result = result.concat(arg);
+                }else if(typeof(arg) === 'object'){
+                    result.push(JSON.stringify(arg));
+                }
+            });
         }
 
         return result;
     }
-}
+};
