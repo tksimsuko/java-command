bda6e30ec1233b4a9e6a8cf83de5c735c36fb58a
Takeshi Takashima <takashima-takeshi@ksk.co.jp>
2017-08-31 18:37:28 +0900

    update

--- java.js
+++ java.js
@@ -1,30 +1,75 @@
-const fs = require('fs');
 const path = require('path');
 const child_process = require('child_process');
 
 //TODO　data文字列化　文字コード指定
 module.exports = function(param){
-    let javaPath = param.java || 'java';
+    let javaPath;
     let jvmOptions = param.jvmOptions || [];
     let classpathArray = param.classpath || [];
-    let tempPath = param.tempPath || '';
+    let tempPath = param.temp || '';
     let cwdPath = param.cwd || __dirname;
     let isBufferMode = param.bufferMode;
 
+    if(param.java){
+        javaPath = path.resolve(cwdPath, param.java);
+    }else{
+        javaPath = 'java';
+    }
+    
     return {
         execute: execute,
+        executeCommand: executeCommand,
         classpath: classpath
     };
 
     ////////// function
-    //Javaコマンドを実行する
-    //@require executableJavaName
-    //第2引数以降：　可変長リストを受け取り、javaコマンドのパラメータとして渡す
-    function execute(executableJavaName){
-        if(!executableJavaName){
-            throw new Error('executableJavaName is required.');
+    //Javaを実行する
+    //@param executableJavaName @required
+    //@param data Javaに渡すデータ(System.inに渡す)
+    function execute(executableJavaName, data){
+        let javaProcess = executeJava(executableJavaName, data);
+        let processHandler = applyEvent(javaProcess);
+
+        if(data){
+            processHandler.systemIn(data);
         }
 
+        return processHandler;
+    }
+    //Javaコマンドを実行する
+    //@param executableJavaName @required
+    //@param 第2引数以降：　可変長リストをjavaコマンドのパラメータとして渡す
+    function executeCommand(executableJavaName){
+        let argArray = Array.prototype.slice.call(arguments);
+        let args = argArray.length > 1 ? argArray.slice(1) : [];
+        let javaProcess = executeJavaCommand(executableJavaName, args);
+        return applyEvent(javaProcess);
+    }
+    //classpthをセットする
+    function classpath(pathParam){
+        if(pathParam){
+            switch(typeof(pathParam)){
+                case 'string':
+                    classpathArray.push(pathParam);
+                    break;
+                case 'object':
+                    classpathArray = classpathArray.concat(pathParam);
+                    break;
+            }
+        }
+    }
+
+
+    function executeJava(executableJavaName){
+        let spawnArgs = generateSpawnArgs(executableJavaName);
+        return child_process.spawn(javaPath, spawnArgs, {cwd: cwdPath, env: {TEMP: tempPath}});
+    }
+    function executeJavaCommand(executableJavaName, args){
+        let command = generateCommand(executableJavaName, args);
+        //shell: false -> パラメータ文字列を空白文字で区切らない　空白文字が含まれていても一つのパラメータとしてjavaに渡す
+        return child_process.exec(command, {cwd: cwdPath, env: {TEMP: tempPath}, shell: false});
+    }
+    function applyEvent(javaProcess){
         let stdoutBuffer = Buffer.from('');
         let stderrBuffer = Buffer.from('');
 
@@ -33,25 +78,19 @@ module.exports = function(param){
         let endCallback = param.end;
         let errorCallback = param.error;
 
-        let argArray = Array.prototype.slice.call(arguments);
-        let args = argArray.length > 1 ? argArray.slice(1) : [];
-
-        let commandArgs = generateCommandArgs(executableJavaName, args);
-        //shell: false -> パラメータ文字列を空白文字で区切らない　空白文字が含まれていても一つのパラメータとしてjavaに渡す
-        let javaProcess = child_process.spawn(javaPath, commandArgs, {cwd: cwdPath, env: {TEMP: tempPath}, shell: false});
 
         //event
         javaProcess.stdout.on('data', function(data){
             if(isBufferMode){
-                stdoutBuffer = Buffer.concat([stdoutBuffer, data]);
+                stdoutBuffer = Buffer.concat([stdoutBuffer, Buffer.from(data)]);
             }
             if(systemOutCallback){
                 systemOutCallback(data);
             }
         });
-        javaProcess.stderr.on('data', function(data){
+        javaProcess.stderr.on('stderr data', function(data){
             if(isBufferMode){
-                stderrBuffer = Buffer.concat([stderrBuffer, data]);
+                stderrBuffer = Buffer.concat([stderrBuffer, Buffer.from(data)]);
             }
             if(systemErrorCallback){
                 systemErrorCallback(data);
@@ -70,6 +109,13 @@ module.exports = function(param){
 
         return {
             childProcess: javaProcess,
+            systemIn: function(data){
+                if(typeof(data) === 'object'){
+                    data = JSON.stringify(data);
+                }
+                javaProcess.stdin.write(data);
+                javaProcess.stdin.end();
+            },
             systemOut: function(callback){
                 if(callback){
                     systemOutCallback = callback;
@@ -96,21 +142,30 @@ module.exports = function(param){
             }
         };
     }
-    function classpath(pathParam){
-        if(pathParam){
-            switch(typeof(pathParam)){
-                case 'string':
-                    classpathArray.push(pathParam);
-                    break;
-                case 'object':
-                    classpathArray = classpathArray.concat(pathParam);
-                    break;
-            }
+    function generateCommand(executableJavaName, args){
+        let command = javaPath;
+        if(jvmOptions && jvmOptions.length > 0){
+            command += ' ' + jvmOptions.join(' ');
         }
-    }
-    function generateCommandArgs(executableJavaName, args){
-        let result = [];
+        if(classpathArray && classpathArray.length > 0){
+            command += ' -classpath';
+            command += ' ' + classpathArray.join(path.delimiter);
+        }
+        command += ' ' + executableJavaName;
 
+        if(args && args.length > 0){
+            args.forEach(function(arg){
+                if(typeof(arg) === 'object'){
+                    command += ' ' + JSON.stringify(arg);
+                }else{
+                    command += ' ' + arg;
+                }
+            });
+        }
+        return command;
+    }
+    function generateSpawnArgs(executableJavaName){
+        let result = [];
         if(jvmOptions && jvmOptions.length > 0){
             result = result.concat(jvmOptions);
         }
@@ -119,16 +174,6 @@ module.exports = function(param){
             result.push(classpathArray.join(path.delimiter));
         }
         result.push(executableJavaName);
-        if(args && args.length > 0){
-            args.forEach(function(arg){
-                if(typeof(arg) === 'string'){
-                    result = result.concat(arg);
-                }else if(typeof(arg) === 'object'){
-                    result.push(JSON.stringify(arg));
-                }
-            });
-        }
-
         return result;
     }
 };
